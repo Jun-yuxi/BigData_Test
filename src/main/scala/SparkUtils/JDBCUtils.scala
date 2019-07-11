210909c7d885c9d69531a3b755daba7dcfb479f8
Jun-yuxi <271670565@qq.com>
2019-07-11 15:32:07 +0800

    第一次提交：dws层

---
+++ src/main/scala/SparkUtils/JDBCUtils.scala
@@ -0,0 +1,20 @@
+package SparkUtils
+
+import java.util.Properties
+
+import config.ConfigManager
+
+/**
+  * JDBC
+  */
+object JDBCUtils {
+
+  def getJdbcProp():(Properties,String)={
+    val prop = new Properties()
+    prop.put("user",ConfigManager.getProper("jdbc.user"))
+    prop.put("password",ConfigManager.getProper("jdbc.password"))
+    prop.put("driver",ConfigManager.getProper("jdbc.driver"))
+    val jdbcUrl = ConfigManager.getProper("jdbc.url")
+    (prop,jdbcUrl)
+  }
+}
