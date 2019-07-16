854f7d57ab21a0e3e2a45780be13e02951312e47
Jun-yuxi <271670565@qq.com>
2019-07-16 09:01:04 +0800

    第二次提交：dm层

--- src/main/scala/Constants/Constan.scala
+++ src/main/scala/Constants/Constan.scala
@@ -5,7 +5,7 @@ package Constants
   */
 object Constan {
   // spark上下文常量
-  val SPARK_APP_NAME_USER ="Teacher"
+  val SPARK_APP_NAME_USER ="Student"
   val SPARK_LOACL = "local"
 
 }
--- src/main/scala/config/ConfigManager.scala
+++ src/main/scala/config/ConfigManager.scala
@@ -8,10 +8,13 @@ object ConfigManager {
   // 通过类加载器方法来加载指定的配置文件
   try{
     val in_dws = ConfigManager.getClass.getClassLoader.getResourceAsStream("dwd_dws.properties")
-
+    val in_dm = ConfigManager.getClass.getClassLoader.getResourceAsStream("dws_dm.properties")
+    val in_dm_visit = ConfigManager.getClass.getClassLoader.getResourceAsStream("dm_user_visit.properties")
     val in_basic = ConfigManager.getClass.getClassLoader.getResourceAsStream("basic.properties")
 
     prop.load(in_dws)
+    prop.load(in_dm_visit)
+    prop.load(in_dm)
     prop.load(in_basic)
   }catch {
     case e:Exception=>e.printStackTrace()
